1dd4355937919ce835949cbc2092ec9be1528d32
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-14 23:07:50 +0630

    update(): json response method changed

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-// Declare a handler which writes a plain-text response with information about the // application status, operating environment and version.
-func (app *application) healthcheckHandler(w http.ResponseWriter, _ *http.Request) {
-	// Create a fixed-format JSON response from a string. Notice how we're using a raw // string literal (enclosed with backticks) so that we can include double-quote
-	// characters in the JSON without needing to escape them? We also use the %q verb to // wrap the interpolated values in double-quotes.
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	// Set the "Content-Type: application/json" header on the response. If you forget to // this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
-	// header instead.
-	w.Header().Set("Content-Type", "application/json")
-	// Write the JSON as the HTTP response body.
-	_, err := w.Write([]byte(js))
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status": "available", "environment": app.config.env, "version": version,
+	}
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		panic(err)
-		return
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
 }
