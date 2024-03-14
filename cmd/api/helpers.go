3c7ddb408206712c815c4dc3e1718d8b678e6f4d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-14 23:34:36 +0630

    update(): use marshal indent instead of marshal

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -21,7 +21,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
