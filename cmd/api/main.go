d84527d0bb80f30a51b7ce4de5cca9998f795f11
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-16 10:12:31 +0630

    feat(db): db connection setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number. Later in the book we'll // generate this automatically at build time, but for now we'll just store the version // number as a hard-coded global constant.
@@ -18,6 +22,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Define an application struct to hold the dependencies for our HTTP handlers, helpers, // and middleware. At the moment this only contains a copy of the config struct and a
@@ -34,9 +41,18 @@ func main() {
 	// corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:password@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 	// Initialize a new structured logger which writes log entries to the standard out // stream.
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	logger.Info("successfully connected to database...")
+
+	defer db.Close()
 	// Declare an instance of the application struct, containing the config struct and // the logger.
 	app := &application{
 		config: cfg,
@@ -54,7 +70,49 @@ func main() {
 	}
 	// Start the HTTP server.
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// openDB returns a sql.DB connection pool to postgres database
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config struct.
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum number of open (in-use + idle) connections in the pool.
+	// Note that passing a value less than or equal to 0 will mean there is no limit.
+	// db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	// Set the maximum number of idle connection in the pool. Again,
+	// passing a value less than or equal to 0 will mean there is no limit
+	// db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// Use the time.ParseDuration() function to convert the idle timeout duration string to a
+	// time.Duration type.
+	// duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// Set the maximum idle timeout.
+	// db.SetConnMaxIdleTime(duration)
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database,
+	// passing in the context we created above as a parameter.
+	// If connection couldn't be established successfully within the 5-second deadline,
+	// then this will return an error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// Return the sql.DB connection pool.
+	return db, nil
 }
