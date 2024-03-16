64d9bb03bf957dd18d610c8f095867b3c5b66582
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-16 12:16:04 +0630

    feat(): model struct and new method added

---
+++ internal/data/models.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	// ErrRecordNotFound is returned when a movie record doesn't exist in database.
+	ErrRecordNotFound = errors.New("record not found")
+
+	// ErrEditConflict is returned when a there is a data race, and we have an edit conflict.
+	ErrEditConflict = errors.New("edit conflict")
+)
+
+type Models struct {
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+	}
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
